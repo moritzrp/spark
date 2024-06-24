a2241dad2199e7c82ea935eb4dfe29232c0e8c81
moritzrp <50995474+moritzrp@users.noreply.github.com>
2024-06-24 15:53:22 +0200

    feat: create polls app

---
+++ spark/polls/views.py
@@ -0,0 +1,57 @@
+from django.db.models import F, QuerySet
+from django.http import HttpRequest, HttpResponse, HttpResponseRedirect
+from django.shortcuts import (
+    get_object_or_404,
+    render,
+)
+from django.urls import reverse
+from django.utils import timezone
+from django.views import generic
+
+from polls.models import Choice, Question
+
+
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+    context_object_name = "latest_question_list"
+
+    def get_queryset(self) -> QuerySet[Question]:
+        """
+        Return the last five published questions
+        (not including those set to be published in the future)
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by(
+            "-pub_date"
+        )[:5]
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "polls/detail.html"
+
+    def get_queryset(self) -> QuerySet[Question]:
+        """
+        Excludes questions that are not published yet
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = "polls/results.html"
+
+
+def vote(request: HttpRequest, question_id: int) -> HttpResponseRedirect | HttpResponse:
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
+    except (KeyError, Choice.DoesNotExist):
+        return render(
+            request,
+            "polls/detail.html",
+            {"question": question, "error_message": "You didn't select a choice."},
+        )
+    else:
+        selected_choice.votes = F("votes") + 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse("polls:results", args=(question_id,)))
